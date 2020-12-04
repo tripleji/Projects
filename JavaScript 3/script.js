72a6a25616e53c1196bf74052655c67d90bae172
tripleji <67589423+tripleji@users.noreply.github.com>
2020-12-04 10:24:05 +0900

    Start the JavaScript 3

---
+++ JavaScript 3/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1EL = document.getElementById('score--1');
+const diceEL =document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1EL.textContent = 0;
+diceEL.classList.add('hidden');
