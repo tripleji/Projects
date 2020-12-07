3aba7b199d569fd9f18b98747ae29e0dfed1f789
tripleji <67589423+tripleji@users.noreply.github.com>
2020-12-07 16:59:05 +0900

    Set up the React

---
+++ React/src/03/TodaysPlan.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TodaysPlan extends React.Component {
+    render() {
+        return (
+            <div className="message-container">
+                 연습하자!
+            </div>
+        );
+    }
+}
+
+export default TodaysPlan;
