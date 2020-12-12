0d10b18ffaa364846078d4b6fbfaf157a3b9a004
tripleji <67589423+tripleji@users.noreply.github.com>
2020-12-12 23:20:46 +0900

    Post

--- jQuery/Fade/js/script.js
+++ jQuery/Fade/js/script.js
@@ -1,8 +1,4 @@
 $(function () {
-  // $("p").css("background-color", "rgba(180, 180, 30, 0.8)");
-  // $(".red-box").css("background-color", "rgba(180, 180, 30, 0.8)");
-  // $("#list").css("background-color", "rgba(180, 180, 30, 0.8)");
-  // $("input[type="text"]").css("background-color", "rgba(180, 180, 30, 0.8)");
-  $("h2, p, input").css("background-color", "rgba(180, 180, 30, 0.8)");
+  $("ul ul:first").append("<li>It's fucked up");
 });
    
